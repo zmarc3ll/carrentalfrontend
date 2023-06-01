941950f6a17012f4967eb498ccda5e0b6772dbf1
zmarc3ll <zubermarcellpetrik@gmail.com>
2023-06-01 18:49:07 +0200

    asd

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Main from './pages/Main';
+import { title } from 'process';
+//import {Helmet} from 'react-helmet';
 
 function App() {
   return (
-    <Main></Main>
+    <><Main></Main></>
   );
 }
 
--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,4 +1,4 @@
-import { Component } from "react";
+import { Component, FormEvent } from "react";
 
  interface Cars {
     id: number;
@@ -15,62 +15,97 @@ interface carResponeList {
 }
 
 interface State {
-    cars: []
+    cars: Cars[],
+    license_plate_number: string,
+    brand: string,
+    model: string,
+    daily_cost: number,
 }
 export default class Main extends Component<{}, State> {
 
     state: State = {
-         cars: []
+         cars: [],
+         license_plate_number:'',
+         brand:'',
+         model:'',
+         daily_cost: 0
      }
 
     async loadCars() {
-        
+        let response = await fetch('http://localhost:3000/api/cars');
+        let data = await response.json() as carResponeList;
+        this.setState({
+            cars: data.cars
+        });
     }
+
+    async handleSubmit(event: FormEvent<HTMLFormElement>) {
+        event.preventDefault();
+
+        const {license_plate_number} = this.state;
+
+        const dbData = {
+            license_plate_number: license_plate_number
+        }
+
+        let response = await fetch('http://localhost:3000/api/cars', {
+            method: 'POST',
+            headers: {  
+                'Content-type': 'application/json'
+            },
+            body: JSON.stringify(dbData),
+        });
+
+        this.setState({
+            license_plate_number: ''
+        })
+    }
+
+    async onLoad() {
+        this.loadCars();
+    }
+
+    componentDidMount() {
+        this.onLoad();
+    }
+
 
     render() {
         return <>
         <div className="container-fluid">
             <nav className="mt-5 ms-5">
-                <a href="#" className="text-decoration-none">Uj Auto felvétele &emsp;</a>
+                <a href="#form" className="text-decoration-none">Új Auto felvétele &emsp;</a>
                 <a href="https://petrik.hu/" className="text-decoration-none">Petrik honlap</a>
                 <h2 className="mt-2">Petrik Autokölcsönző</h2>
             </nav>
-            <div className="col-md-12">
                 <div className="row mt-4 ms-3 me-3">
+                    {this.state.cars.map((cars: Cars) => (
                     <div className="col-lg-4 col-md-4 col-sm-4">
-                        <div className="card">
+                        <div className="card mt-3 ms-1 me-1">
                             <div className="cord-body">
-                                <h3>IOA-699</h3>
-                                <p>Márka: ford</p>
-                                <p>Modell: Chille</p>
-                                <p>Napidij: 24000 Ft</p>
+                                <h3>{cars.license_plate_number}</h3>
+                                <p>Márka: {cars.brand}</p>
+                                <p>Modell: {cars.model}</p>
+                                <p>Napidij: {cars.daily_cost} Ft</p>
                                 <img src={'images/ford_chile.png'} alt="" className="img-fluid" />
                             </div>
                         </div>
                     </div>
-                    <div className="col-lg-4 col-md-4 col-sm-4">
-                        <div className="card">
-                            <div className="cord-body">
-                                <h3>IOA-699</h3>
-                                <p>Márka: ford</p>
-                                <p>Modell: Chille</p>
-                                <p>Napidij: 24000 Ft</p>
-                                <img src={'images/ford_chile.png'} alt="" className="img-fluid" />
-                            </div>
-                        </div>
-                    </div>
-                    <div className="col-lg-4 col-md-4 col-sm-4">
-                        <div className="card">
-                            <div className="cord-body">
-                                <h3>IOA-699</h3>
-                                <p>Márka: ford</p>
-                                <p>Modell: Chille</p>
-                                <p>Napidij: 24000 Ft</p>
-                                <img src={'images/ford_chile.png'} alt="" className="img-fluid" />
-                            </div>
-                        </div>
-                    </div>
-                </div>
+                    ))}
+            </div>
+            <div className="d-block mt-5" id="form">
+                <h3 className="text-center">Új autó felvétele</h3>
+                <form className="form text-center" onSubmit={this.handleSubmit}>
+                        <label htmlFor="plate">Rendszám</label><br />
+                        <input type="text" id="plate" required className="form-control mt-2" value={this.state.license_plate_number} onChange={e => this.setState({license_plate_number: e.currentTarget.value})}/>
+                        <label htmlFor="brand">Márka</label><br />
+                        <input type="text" id="brand" className="form-control mt-2"/>
+                        <label htmlFor="model">Modell</label><br />
+                        <input type="text" id="model" className="form-control mt-2" />
+                        <label htmlFor="dij">Napidij</label><br />
+                        <input type="number" id="dij" className="form-control mt-2" />
+                        <button type="submit" className="btn btn-dark mt-4">Felvétel</button>
+                </form>
             </div>
             <footer className="mt-5 ms-5">
                 <p>Készítette: Zuber Marcell</p>
